1c90c7efc24adc4fadbf682386b2f0ed1f2ad962
mateusz <mateuszalmannai@googlemail.com>
2015-07-12 18:46:46 +0200

    spec out page navigation

---
+++ spec/support/attributes.rb
@@ -0,0 +1,13 @@
+# pass in an overrides parameter, who's default is is an empty hash.
+# However, if a hash is passed in as a parameter, it will get merged
+# with the set of attributes in the method body.
+# This lets us override any of the attribute values very easily if
+# necessary.
+def event_attributes(overrides = {}){
+  name: "BugSmash",
+  location: "Denver",
+  price: 10.00,
+  description: 'A fun evening of bug smashing!',
+  starts_at: 10.days.from_now
+}.merge(overrides)
+end
