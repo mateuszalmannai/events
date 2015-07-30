292ac5bf150e307a5900867bbc35ca471cdf3f1f
mateusz <mateuszalmannai@googlemail.com>
2015-07-30 20:50:52 +0200

    add create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "events#index"
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  # no link to update action since form automatically posts there
-  patch "events/:id" => "events#update"
+  # get "events" => "events#index"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # # no link to update action since form automatically posts there
+  # patch "events/:id" => "events#update"
+  resources :events
 end
