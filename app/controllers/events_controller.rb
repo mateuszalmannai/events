700ab1d34744183c8b5f7927f46c11e11e84a56f
mateusz <mateuszalmannai@googlemail.com>
2015-08-24 21:10:31 +0200

    add flash message for deletion

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -39,7 +39,7 @@ class EventsController < ApplicationController
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
-    redirect_to events_url
+    redirect_to events_url, notice: "Event successfully deleted!"
   end
 
   private
